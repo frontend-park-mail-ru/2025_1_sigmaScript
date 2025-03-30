f83ffb07b2ddb8285d926d1e10d54368b3d9c28d
SkorikovGeorge <skorikovgk@yandex.ru>
2025-03-30 13:29:31 +0300

    fixed host address

--- public/consts.js
+++ public/consts.js
@@ -1,5 +1,5 @@
 export const BACKEND_PORT = 8080;
-export const HOST = 'localhost';
+export const HOST = '217.16.20.177';
 export const AUTH_URL = `http://${HOST}:${BACKEND_PORT}/auth/`;
 export const ERRORS = {
   ErrParseJSONShort: 'parse_json_error',
